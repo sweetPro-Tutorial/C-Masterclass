7bc9ca4f1aba1b0f22d14211f35ad9852847fd24
sweetPro-Tutorial <sbyang@mirisoft.co.kr>
2023-04-12 09:42:11 +0900

    add bash profile

--- 1.hello-world/helloworld.c
+++ 1.hello-world/helloworld.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world.\n");
+    printf("Hello world. 한글!\n");
 }
---
+++ settings/.bash_profile
@@ -0,0 +1,3 @@
+chcp.com 65001
+export PS1="\[\033[32m\]C-Masterclass@ \[\033[33m\]\w \[\033[0m\]$ "
+cd ~
