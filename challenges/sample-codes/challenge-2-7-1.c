c024c5aeba0562462605528d4b85b970e8c036ab
sweetPro-Tutorial <sbyang@mirisoft.co.kr>
2023-04-16 10:47:00 +0900

    add challenge

---
+++ challenges/sample-codes/challenge-2-7-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double fahrenheit = 400.0;
+    double celsius = 0.0;
+
+    // 계산식: fahrenheit --> celsius
+    celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+    printf("Fahrenheit %6.1f --> Celsius %6.1f\n", fahrenheit, celsius);
+
+    return 0;
+}
