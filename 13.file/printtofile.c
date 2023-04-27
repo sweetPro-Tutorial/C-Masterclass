578e69fca533846f58e98c26514547d13ed45f32
sweetPro-Tutorial <sbyang@mirisoft.co.kr>
2023-04-27 20:16:05 +0900

    add printtofile

--- 13.file/printtofile.c
+++ 13.file/printtofile.c
@@ -7,14 +7,24 @@
 #include <stdio.h>
 
 int main() {
+    printf("Hello world!\n");
+    fprintf(stdout, "Hello world!\n");
+
     // 파일 열기
-    FILE *stream;  
+    FILE *stream;  // 파일 포인터용 변수 선언
     char filename[100] = "myfirstfile.txt";
     stream = fopen(filename, "w");
+    // 예외 처리 로직
+    if(stream == NULL) {
+        printf("파일 %s 을 여는데 실패했습니다\n", filename);
+        return 0;
+    }
 
     // 파일에 출력
     fprintf(stream, "Hello world!\n");
 
     // 파일 닫기
     fclose(stream);
+
+    return 0;
 }
