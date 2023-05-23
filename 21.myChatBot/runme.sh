0fca93a5e7a53cd9d2610d889d9b49e19951282e
sweetPro-Tutorial <sbyang@mirisoft.co.kr>
2023-05-23 12:30:58 +0900

    deleted:    21.myChatBot/common.c 	deleted:    21.myChatBot/common.h 	deleted:    21.myChatBot/list.c 	deleted:    21.myChatBot/list.h 	modified:   21.myChatBot/main.c 	deleted:    21.myChatBot/response-parser.c 	deleted:    21.myChatBot/response-parser.h 	deleted:    21.myChatBot/response.json 	deleted:    21.myChatBot/runme.sh 	deleted:    21.myChatBot/sample.json

--- 21.myChatBot/runme.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-curl   https://api.openai.com/v1/chat/completions  \
--H "Content-Type: application/json"  \
--H "Authorization: Bearer $OPENAI_API_KEY"  \
--d "$(cat ~/cprojects/21.myChatBot/myquestion.json)" > ~/cprojects/21.myChatBot/response.json
